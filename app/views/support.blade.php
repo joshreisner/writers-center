9d613de97e53d0b5ef8f628953e94b7109cde3d3
Josh Reisner <josh@joshreisner.com>
2014-06-15 19:48:12 -0700

    working on stripe billing

--- app/views/support.blade.php
+++ app/views/support.blade.php
@@ -4,60 +4,28 @@
 
 	<h1>{{ $title }}</h1>
 
-	<div class="alert alert-warning">Already a member? Please <a class="login alert-link">log in</a>.</div>
+	{{ Form::open() }}
+		<div class="row">
+			<div class="col-md-10">
+				{{ Form::text(null, null, ['class'=>'form-control', 'data-stripe'=>'number', 'placeholder'=>'Card #']) }}
+			</div>
+			<div class="col-md-2">
+				{{ Form::text(null, null, ['class'=>'form-control', 'data-stripe'=>'cvc', 'placeholder'=>'CVC']) }}
+			</div>
+		</div>
 
-	<form class="form-horizontal">
-		<fieldset>
-			<legend>Register</legend>
-			<div class="form-group">
-				<label for="first_name" class="col-sm-2 control-label">First Name</label>
-				<div class="col-sm-10">
-					<input type="text" class="form-control" id="first_name" placeholder="First Name">
-				</div>
+		<div class="row">
+			<div class="col-md-6">
+				{{ Form::selectMonth(null, null, ['class'=>'form-control', 'data-stripe'=>'exp-month']) }}
 			</div>
-			<div class="form-group">
-				<label for="last_name" class="col-sm-2 control-label">Last Name</label>
-				<div class="col-sm-10">
-					<input type="text" class="form-control" id="last_name" placeholder="Last Name">
-				</div>
+			<div class="col-md-6">
+				{{ Form::selectYear(null, date('Y'), date('Y') + 10, null, ['class'=>'form-control', 'data-stripe'=>'exp-year']) }}
 			</div>
-			<div class="form-group">
-				<label for="email" class="col-sm-2 control-label">Email</label>
-				<div class="col-sm-10">
-					<input type="email" class="form-control" id="email" placeholder="Email">
-				</div>
-			</div>
-			<div class="form-group">
-				<label for="password" class="col-sm-2 control-label">Password</label>
-				<div class="col-sm-10">
-					<input type="password" class="form-control" id="password" placeholder="Password">
-				</div>
-			</div>
-			<div class="form-group">
-				<label for="address" class="col-sm-2 control-label">Address</label>
-				<div class="col-sm-10">
-					<input type="text" class="form-control" id="address" placeholder="Address">
-				</div>
-			</div>
-			<div class="form-group">
-				<label for="city" class="col-sm-2 control-label"></label>
-				<div class="col-sm-5">
-					<input type="text" class="form-control" id="city" placeholder="City">
-				</div>
-				<div class="col-sm-3">
-					{{ Form::dropdown('state', $states, 'NY') }}
-				</div>
-				<div class="col-sm-2">
-					<input type="text" class="form-control" id="zip" placeholder="ZIP">
-				</div>
-			</div>
-			<div class="form-group">
-				<div class="col-sm-offset-2 col-sm-10">
-					<input type="submit" class="btn btn-primary" value="Register">
-				</div>
-			</div>
-		</fieldset>
-	</form>
+		</div>
+
+		{{ Form::submit('Register', ['class'=>'btn btn-primary']) }}
+
+	{{ Form::close() }}
 
 @endsection
 
